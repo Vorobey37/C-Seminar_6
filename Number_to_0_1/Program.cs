39295eb605b8f4de8fe7444ec572b905ec46aa1e
Abvorobey <abvorobey@gmail.com>
2023-03-01 22:48:54 +0300

    Конвертация тесятичного числа

---
+++ Number_to_0_1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу,
+которая будет преобразовывать десятичное число в двоичное.
+
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+Console.Clear();
+Console.WriteLine("Введите число:");
+int decNumber = int.Parse(Console.ReadLine());
+
+string DecToNumber(int decNumber, int otherSystem)
+{
+    string res = "";
+    string nums = "0123456789ABCDEF";
+    while(decNumber>0)
+    {
+        int ost = decNumber/otherSystem;
+        res = nums[decNumber-otherSystem*ost] + res;
+        decNumber/=otherSystem;
+    }
+    return res;
+}
+string res = DecToNumber(decNumber, 2);
+Console.WriteLine($"{decNumber} -> {res}");
